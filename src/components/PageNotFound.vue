fbfc16ff20e6a5d824f88c54d37f1dc37f0a62fc
wamalwa <wssilverpg@gmail.com>
2022-09-24 09:44:29 +0300

    404 to index

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -1,6 +1,6 @@
 Vue.component("page-not-found", {
     template: "",
     created: function() {
-        window.location.href = "/page-404.html";
+        window.location.href = "/index.html";
     }
 }
